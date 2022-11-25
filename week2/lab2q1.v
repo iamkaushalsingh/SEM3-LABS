4fb541e563102b43d28b198602bda4c250b22d09
Kaushal Singh <55918941+iamkaushalsingh@users.noreply.github.com>
2022-11-25 21:25:26 +0530

    Delete week2 directory

--- week2/lab2q1.v
+++
@@ -1,5 +0,0 @@
-module lab2q1(a,b,c,d,f);
-input a,b,c,d;
-output f;
-assign f =(~a&b)|(c&d)|(~b&c)|(b&~c&~d);
-endmodule
--- week2/q2b.v
+++
@@ -1,5 +0,0 @@
-module q2b(a,b,c,d,f);
-input a,b,c,d;
-output f;
-assign f =(~b|c|d)&(~a|c|~d)&(~a|b|~c)&(a|~b|~c);
-endmodule
