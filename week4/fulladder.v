6c45912cf8ea5c8491f22352257d612e7a8c8445
Kaushal Singh <55918941+iamkaushalsingh@users.noreply.github.com>
2022-11-25 21:25:55 +0530

    Delete week4 directory

--- week4/fulladder.v
+++
@@ -1,6 +0,0 @@
-module fulladder(cin,x,y,sum,cout);
-input cin,x,y;
-output sum,cout;
-assign sum = x^y^cin;
-assign cout = (x&y)|(x&cin)|(y&cin);
-endmodule                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
